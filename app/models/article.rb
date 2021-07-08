b35d5b66c079dc6f0483ff39b8c878cef9c1a8f8
AnneCecile Mechin <ac.mechin@gmail.com>
2021-07-08 16:49:42 +0200

    simple index page

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,4 +5,9 @@ class Article < ApplicationRecord
     message: "%{value} is not a valid argument" }
   has_rich_text :rich_body
   has_one_attached :photo
+
+
+  def short_description
+    self.rich_body.to_plain_text.truncate(80)
+  end
 end
